c356f4abef49486378685972c5377c64b028069e
mkwan <wan@example.com>
2023-06-22 00:26:42 +0300

    functions and headers

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 : Entry point
+ * print_alphabet_x10 - Entry point
  *
  * print_alphabet_x10 - print all the lowercase letters ten times.
  *
- * Return: void
+ * Return: void 0.
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	int k;
-	
-	for (k = 0; k <= 10; k++)
+
+	for (k = 0; k < 10; k++)
 	{
 		for (i = 97; i < 123; i++)
 		{
