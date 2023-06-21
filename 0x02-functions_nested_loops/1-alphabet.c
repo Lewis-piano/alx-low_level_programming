fec59a0e8327049a88ad7eac40646af2fcfb8f75
mkwan <wan@example.com>
2023-06-22 00:06:06 +0300

    functions and headers

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
-/** 
+/**
+ * print_alphabet - Entry point
+ *
  * print-alphabets - prints all the lowercase letters
+ *
  * Return: void 0.
  */
-void print_alphabet(void){
-	
+void print_alphabet(void)
+{
 	int i;
-	
-	for (i = 97; i < 122; i++){
+
+	for (i = 97; i < 122; i++)
+	{
 		_putchar(i);
 	}
 	_putchar('\n');
